e5deb8dbcf05f7a03471e075829e745b14295bfd
asdfghj785 <351105687@qq.com>
2021-02-24 21:02:13 +0800

    Update jukan.js

--- jukan.js
+++ jukan.js
@@ -11,7 +11,7 @@ hostname = www.xiaodouzhuan.cn
 
 
 */
-let SB = "123";
+
 const $ = new Env('聚看点')
 const drawcash = $.getdata('jukan_cash') || "30" //提现金额
 const wxname = $.getdata('jukan_name') || ""//微信真实名字，可以在双引号内填入
@@ -20,7 +20,7 @@ let bodys = $.getdata('jukan_body')
 let signtimes = $.getdata('jukan_times')
 let cashout = $.getdata('jukan_out')|| false
 let UA = 'JuKanDian/5.6.5 (iPhone; iOS 14.2; Scale/3.00)'
-let taskresult = "",sumnotify ="";
+let taskresult = "",sb="",sumnotify ="";
 
 if ($.isNode()) {
   if (process.env.JUKAN_BODY && process.env.JUKAN_BODY.indexOf('&') > -1) {
